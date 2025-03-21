2c36308dccd7ade025a88a56ca9d16448b4f8ae0
Nguyen Minh Hieu <lotous12a4@gmail.com>
2025-03-21 13:46:04 +0700

    Update code

---
+++ assets/js/scroll-navbar.js
@@ -0,0 +1,6 @@
+$(".menungang .menu > li > a").removeAttr("href");
+
+$('.item-991').click(function(){
+    $('html, body').animate({
+        scrollTop: $('.extra5').offset().top - 50}, 1000);
+});
--- language.php
+++ language.php
@@ -1,2 +1,6 @@
 <!-- Module bài viết -->
 K2_READ_MORE="Xem thêm"
+
+<!-- Date time blog -->
+K2_DATE_FORMAT_LC2="d/m/Y - H:i"
+<div class="catItemDateCreated"><i class="far fa-clock"></i><?php echo JHTML::_('date', $item->created, JText::_('K2_DATE_FORMAT_LC2')); ?></div>
