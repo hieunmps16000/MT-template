11f07310c0c4ca3cc96ef056a61d024f4aee4147
Nguyen Minh Hieu <lotous12a4@gmail.com>
2025-08-25 15:18:17 +0700

    Update

--- assets/js/handleSearch.js
+++ assets/js/handleSearch.js
@@ -3,13 +3,13 @@ function handleSearch(selector) {
     const $searchSection = $(selector);
 
     $searchSection.on('click', '.workshome-title', function() {
-        $('#bd').addClass('overflow-y-hidden');
+        $('#bd').addClass('overflow-hidden');
         $searchSection.find('.workshome-content').addClass('active');
     });
 
     $searchSection.on('click', '.workshome-content', function() {
         $(this).removeClass('active');
-        $('#bd').removeClass('overflow-y-hidden');
+        $('#bd').removeClass('overflow-hidden');
     });
 
     $searchSection.on('click', 'form', function(e) {
